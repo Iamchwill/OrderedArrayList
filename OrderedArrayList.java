298b9ea71d71dc13c1b3a3e074f72c736d10faa5
iamchwill <wchen20@stuy.edu>
2020-12-12 20:28:18 -0500

    Created helper function and overrode the two add methods to sort

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,4 +7,21 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int capacity){
     super(capacity);
   }
+
+  public boolean add(T item){
+    super.add(this.orderIndex(item), item);
+    return true;
+  }
+
+  public void add(int index, T item){
+    super.add(this.orderIndex(item), item);
+  }
+
+  private int orderIndex(T item){
+    if (item == null) throw new IllegalArgumentException();
+    for (int index = 0; index < this.size(); index++){
+      if (this.get(index).compareTo(item) >= 0) return index;
+    }
+    return this.size();
+  }
 }
